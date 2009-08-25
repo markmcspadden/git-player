8c28c484504c5a0456494c15d8b2e16b5773a051
Mark McSpadden <markmcspadden@gmail.com>
2009-08-25 17:42:27 -0500

    Switch to grit and get commits in GitPlayer#play

--- lib/git_player.rb
+++ lib/git_player.rb
@@ -1,16 +1,26 @@
 require 'rubygems'
-require 'git'
-
+require 'grit'
 
 class GitPlayer
   
-  attr_accessor :start_commit, :end_commit
+  attr_accessor :repo_dir, :start_commit, :end_commit
   
-  # GitPlayer.new(:start_commit => "bafs1234", :end_commit => "67dfjcv0")
+  # GitPlayer.new(:repo_dir => "/Users/mark/Development/git_player_test",
+  #               :start_commit => "bafs1234", 
+  #               :end_commit => "67dfjcv0")
   def initialize(attrs={})
     attrs.each_pair do |k,v|
       instance_variable_set("@#{k}", v)
     end
+  end
+  
+  def play
+    repo = Grit::Repo.new(repo_dir)
+    commits = repo.commits
+    
+    puts repo
+    puts commits
+    commits
   end
   
 end
--- spec/git_player_spec.rb
+++ spec/git_player_spec.rb
@@ -2,18 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe GitPlayer do
   before(:each) do
-    @gp = GitPlayer.new
+    @repo_dir = "/Users/mark/Development/git_player_test"
+    @gp = GitPlayer.new(:repo_dir => @repo_dir,
+                          :start_commit => "bafs1234", 
+                          :end_commit => "67dfjcv0")
   end
   
   describe "initialize" do
     before(:each) do
-      @gp = GitPlayer.new(:start_commit => "bafs1234", :end_commit => "67dfjcv0")
+      @gp = GitPlayer.new(:repo_dir => @repo_dir,
+                            :start_commit => "bafs1234", 
+                            :end_commit => "67dfjcv0")
     end
-    
+
+    it "should set the repo dir from initalization params" do
+      @gp.repo_dir.should == @repo_dir
+    end    
     it "should set the start commit from initalization params" do
       @gp.start_commit.should == "bafs1234"
     end
-    it "should set the start commit from initalization params" do
+    it "should set the end commit from initalization params" do
       @gp.end_commit.should == "67dfjcv0"
     end
   end # initialize
@@ -25,10 +33,33 @@ describe GitPlayer do
     it "should have an end_commit" do
       @gp.should respond_to("end_commit")      
     end
+    it "should have a repo dir" do
+      @gp.should respond_to("repo_dir")
+    end
   end
   
   describe "play" do
-    it "should description" do
+    before(:each) do
+      @mock_repo = mock(Grit::Repo, :commits => [])
+    end
+    
+    it "should fetch the repo" do
+      Grit::Repo.should_receive(:new).with(@repo_dir).and_return(@mock_repo)
+      
+      @gp.play
+    end
+    
+    it "should get the commits" do
+      Grit::Repo.stub!(:new).and_return(@mock_repo)
+      
+      @mock_repo.should_receive(:commits)
+      @gp.play
+    end
+    
+    it "should return an array of commit objects" do
+      @gp_play = @gp.play
+      @gp_play.should be_a(Array)
+      @gp_play.first.should be_a(Grit::Commit)
       
     end
   end # playback
