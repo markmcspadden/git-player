80464003d21694043091ab6fd96183e26b63faf2
Mark McSpadden <markmcspadden@gmail.com>
2009-08-25 16:23:17 -0500

    Add some attributes and smart initialization to GitPlayer

--- lib/git_player.rb
+++ lib/git_player.rb
@@ -1,3 +1,16 @@
+require 'rubygems'
+require 'git'
+
+
 class GitPlayer
+  
+  attr_accessor :start_commit, :end_commit
+  
+  # GitPlayer.new(:start_commit => "bafs1234", :end_commit => "67dfjcv0")
+  def initialize(attrs={})
+    attrs.each_pair do |k,v|
+      instance_variable_set("@#{k}", v)
+    end
+  end
   
 end
--- spec/git_player_spec.rb
+++ spec/git_player_spec.rb
@@ -1,5 +1,36 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe GitPlayer do
+  before(:each) do
+    @gp = GitPlayer.new
+  end
   
-end
+  describe "initialize" do
+    before(:each) do
+      @gp = GitPlayer.new(:start_commit => "bafs1234", :end_commit => "67dfjcv0")
+    end
+    
+    it "should set the start commit from initalization params" do
+      @gp.start_commit.should == "bafs1234"
+    end
+    it "should set the start commit from initalization params" do
+      @gp.end_commit.should == "67dfjcv0"
+    end
+  end # initialize
+  
+  describe "attributes" do
+    it "should have a start_commit" do
+      @gp.should respond_to("start_commit")
+    end
+    it "should have an end_commit" do
+      @gp.should respond_to("end_commit")      
+    end
+  end
+  
+  describe "play" do
+    it "should description" do
+      
+    end
+  end # playback
+  
+end # GitPlayer
